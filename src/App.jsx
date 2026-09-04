23480ce55a2e054b32a7d24528c0172f204dc3d6
Jay Vaghela <jayvaghela@hmail.com>
2026-09-04 12:39:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+import Home from "./componets/Home.jsx";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/componets/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-center oklch(56% 0.021 213.5) text-2xl font-semibold">
+        This project can deploy on AWS EKS
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
